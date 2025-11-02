7815712410f05caa9f3b2da82f7337dd2ed7e93b
Gunin Bhardwaj <164526722+Bhardwaj2809@users.noreply.github.com>
2025-11-02 23:39:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,5 @@ def chat():
         reply = "Alright, noted."
 
     return jsonify({"sentiment": sentiment, "response": reply})
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
